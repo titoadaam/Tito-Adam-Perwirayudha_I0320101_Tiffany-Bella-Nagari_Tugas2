e162d958ef13d4fd5f304d2ff5138388a0b45388
Titoadam <inititoadamperwirayudha@gmail.com>
2021-03-09 20:05:21 +0700

    EXERCISE 2.2

---
+++ EX2.2.py
@@ -0,0 +1,34 @@
+#TITO ADAM PERWIRAYUDHA
+#NIM I0320101
+#KELAS C
+#EXERCISE 2.2
+
+#proses memasukan data ke dalam variabel
+nama = "Tito Adam P"
+#proses mencetak variabel
+print(nama)
+
+#nilai dan tipe data dalam variabel dapat diubah
+umur = 19               #nilai awal
+print(umur)             #mencetak nilai umur
+umur = "sembilan belas" #nilai setelah diubah
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+
+namaDepan = "Tito"
+namaBelakang = "Adam Perwirayudha"
+nama = namaDepan + " " + namaBelakang
+umur = 19
+hobi = "Belajar"
+print("Biodata\n", nama, "\n", umur, "\n", hobi)
+
+#contoh variabel lainya
+inivariabel = "Halo"
+ini_juga_variabel = "hai"
+_inivariabeljuga = "Hi"
+inivariabel222 = "Bye"
+
+panjang = 10
+lebar = 5
+luas = panjang * lebar
+print(luas)
