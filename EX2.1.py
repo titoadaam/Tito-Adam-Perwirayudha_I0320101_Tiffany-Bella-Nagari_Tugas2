20cfc70bd9e55c842f97d38644fe82f65f2d850b
Titoadam <inititoadamperwirayudha@gmail.com>
2021-03-09 19:58:12 +0700

    EXERCISE 2.1

---
+++ EX2.1.py
@@ -0,0 +1,34 @@
+#TITO ADAM PERWIRAYUDHA
+#NIM I0320101
+#KELAS C
+#EXERCISE 2.1
+#-----------------------------------------------------------------
+#TIPE DATA BOOLEAN
+x = True
+print(x)
+print(type(x))
+#-----------------------------------------------------------------
+#TIPE DATA STRING
+x = "Saya sangat senang sekali belajar Python"
+print(x)
+print(type(x))
+#-----------------------------------------------------------------
+#TIPE DATA INTEGER
+x = 32
+print(x)
+print(type(x))
+#-----------------------------------------------------------------
+#TIPE DATA FLOAT
+x = 3.14
+print(x)
+print(type(x))
+#-----------------------------------------------------------------
+#TIPE DATA HEXADECIMAL
+x = hex(54)
+print(x)
+print(type(x))
+#-----------------------------------------------------------------
+#TIPE DATA COMPLEX
+x = (10j)
+print(x)
+print(type(x))
