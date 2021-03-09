384ae410f443bdfab073c1a16562b5f5b382eec5
Titoadam <inititoadamperwirayudha@gmail.com>
2021-03-09 22:12:27 +0700

    EXERCISE 2.2

---
+++ EX2.3.py
@@ -0,0 +1,20 @@
+#TITO ADAM PERWIRAYUDHA
+#NIM I0320101
+#KELAS C
+#EXERCISE 2.3
+
+#menampilkan informasi program
+print("Las dan keliling lingkaran")
+
+#input nilai jari-jari
+r = float(input("Masukan nilai Jari-jari: "))
+
+#Menghitung luas lingkaran
+luas_lingkaran = 3.14 (r ** 2)
+
+#menghitung keliling lingkaran
+keliling_lingkaran = 2 * 3.14 * r
+
+#menampilkan hasil perhitungan ke layar
+pring("luas lingkaran: ", luas_lingkaran)
+print("keliling lingkaran: ", keliling_lingkaran)
