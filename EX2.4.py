a8af2a2241827fc1ab2d13d5357fe5824cc41cd0
Titoadam <inititoadamperwirayudha@gmail.com>
2021-03-09 22:17:09 +0700

    EXERCISE 2.4

---
+++ EX2.4.py
@@ -0,0 +1,19 @@
+#TITO ADAM PERWIRAYUDHA
+#NIM I0320101
+#KELAS C
+#EXERCISE 2.4
+
+#menampilkan informasi program
+
+print("Konversi suhu (farenheit ke celcius")
+
+#input suhu dalam farenheit
+F = float(input("masukan suhu(farenheit): "))
+
+#melakukan konversi suhu ke celcius
+C = 5 * (F-32) / 9
+
+#menampilkan hail konversi ke layar
+
+print("Farenheit: ", F)
+print("Celcius" ,C)
